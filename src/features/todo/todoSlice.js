64fcb521442e97a8ecfd8c3e195cfacd8f69c6f1
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-10-15 15:03:09 +0500

    setting1

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-  todos: JSON.parse(localStorage.getItem("todos")),
+  todos: JSON.parse(localStorage.getItem("todos")) || [],
 };
 
 export const todoSlice = createSlice({
